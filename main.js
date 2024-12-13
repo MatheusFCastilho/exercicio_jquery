797ff7ad31c8f230a34a21f662e0e3fb12c780d0
Matheus Castilho <Matheus@teste.com>
2024-12-13 20:43:45 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('#task-form').on('submit', function (event) {
+        event.preventDefault();
+
+        const taskText = $('#task-input').val();
+        if (taskText) {
+            $('#task-list').append(`<li>${taskText}</li>`);
+            $('#task-input').val('');
+        }
+    });
+
+    $('#task-list').on('click', 'li', function () {
+        $(this).toggleClass('completed');
+    });
+
+    $('#clear-tasks').on('click', function () {
+        $('#task-list').empty();
+    });
+});
